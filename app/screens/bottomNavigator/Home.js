6d9e20a8748b2a5e0721152d4c008457bc06c914
Himanshu <himanshu16315@gmail.com>
2023-02-26 20:34:29 -0500

    Implemented User Screen ⚡️

--- app/screens/bottomNavigator/Home.js
+++ app/screens/bottomNavigator/Home.js
@@ -6,19 +6,17 @@ const Home = () => {
     <View className="w-full h-screen bg-white">
       <SafeAreaView>
         <View className="w-full h-full">
-          {/********* Upper View **********/}
-          <View className="h-[70vh] flex w-full justify-start items-start px-4 py-2">
-            {/********* Title Text **********/}
+          <View className="h-full w-full p-4">
             <Text className="text-2xl font-Raleway tracking-wider px-4 py-4">
-              Home
+              Account
             </Text>
 
-            {/********* User View **********/}
-            <View className="bg-[#D9ED92] py-6 px-2 w-full rounded-tl-3xl rounded-br-3xl flex flex-row justify-between items-center mt-4">
+            {/*********** Card View ***********/}
+            <View className="bg-[#D9ED92] py-6 px-2 w-full rounded-tl-3xl rounded-br-3xl flex flex-row justify-between items-center mt-4 mb-8">
               <View className="px-4 py-2">
-                <Text className="text-xl tracking-wider pb-2">Hi Username</Text>
+                <Text className="text-xl tracking-wider pb-2">Username</Text>
                 <Text className="text-xsm text-gray-500">
-                  Let's begin your journey
+                  Thanks for being a loyal user
                 </Text>
               </View>
               <View className="px-4">
--- app/screens/bottomNavigator/Profile.js
+++ app/screens/bottomNavigator/Profile.js
@@ -55,37 +55,86 @@ const Profile = ({navigation}) => {
     <View className="w-full h-screen bg-white">
       <SafeAreaView>
         <View className="w-full h-full">
-          {/********* Title View **********/}
-          <View className="h-[70vh] flex w-full justify-start items-start px-4 py-2">
-            <View className="h-full w-full">
-              <Text className="text-2xl font-Raleway tracking-wider px-4 py-4">
-                Account
-              </Text>
+          <View className="h-full w-full p-4">
+            <Text className="text-2xl font-Raleway tracking-wider px-4 py-4">
+              Account
+            </Text>
 
-              <View className="bg-[#D9ED92] py-6 px-2 w-full rounded-tl-3xl rounded-br-3xl flex flex-row justify-between items-center mt-4">
-                <View className="px-4 py-2">
-                  <Text className="text-xl tracking-wider pb-2">Username</Text>
-                  <Text className="text-xsm text-gray-500">
-                    Thanks for being a loyal user
-                  </Text>
-                </View>
-                <View className="px-4">
-                  <Image
-                    source={require('../../../assets/images/man.png')}
-                    className="h-16 w-16"
-                  />
-                </View>
+            {/*********** Card View ***********/}
+            <View className="bg-[#D9ED92] py-6 px-2 w-full rounded-tl-3xl rounded-br-3xl flex flex-row justify-between items-center mt-4 mb-8">
+              <View className="px-4 py-2">
+                <Text className="text-xl tracking-wider pb-2">Username</Text>
+                <Text className="text-xsm text-gray-500">
+                  Thanks for being a loyal user
+                </Text>
+              </View>
+              <View className="px-4">
+                <Image
+                  source={require('../../../assets/images/man.png')}
+                  className="h-16 w-16"
+                />
               </View>
             </View>
-          </View>
 
-          {/********* Logout View **********/}
-          <View className="flex w-full justify-center items-center px-4 py-6">
-            <TouchableOpacity onPress={() => showAlert()}>
-              <Text className="text-lg tracking-wide text-[#168AAD]">
-                Log out
-              </Text>
-            </TouchableOpacity>
+            {/*********** Links View ***********/}
+            <View className="m-2 flex flex-col space-y-6">
+              <TouchableOpacity className="flex flex-row items-center justify-between border-b border-gray-100 pb-4">
+                <View className="flex flex-row items-center space-x-4">
+                  <Image
+                    source={require('../../../assets/images/password.png')}
+                    className="h-6 w-6"
+                  />
+                  <Text className="text-lg font-light">Change Password</Text>
+                </View>
+                <Image
+                  source={require('../../../assets/images/next.png')}
+                  className="h-6 w-6"
+                />
+              </TouchableOpacity>
+
+              <TouchableOpacity className="flex flex-row items-center justify-between border-b border-gray-100 pb-4">
+                <View className="flex flex-row items-center space-x-4">
+                  <Image
+                    source={require('../../../assets/images/star.png')}
+                    className="h-6 w-6"
+                  />
+                  <Text className="text-lg font-light">Rate Us</Text>
+                </View>
+                <Image
+                  source={require('../../../assets/images/next.png')}
+                  className="h-6 w-6"
+                />
+              </TouchableOpacity>
+
+              <TouchableOpacity className="flex flex-row items-center justify-between">
+                <View className="flex flex-row items-center space-x-4">
+                  <Image
+                    source={require('../../../assets/images/friend.png')}
+                    className="h-6 w-6"
+                  />
+                  <Text className="text-lg font-light">Tell Your Friend</Text>
+                </View>
+                <Image
+                  source={require('../../../assets/images/next.png')}
+                  className="h-6 w-6"
+                />
+              </TouchableOpacity>
+            </View>
+
+            {/*********** Logout View ***********/}
+            <View className="flex w-full h- justify-center items-center px-4 py-6 mt-[30%] border-t border-b border-gray-100">
+              <TouchableOpacity
+                onPress={() => showAlert()}
+                className="flex flex-row items-center space-x-4">
+                <Image
+                  source={require('../../../assets/images/off.png')}
+                  className="h-5 w-5"
+                />
+                <Text className="text-lg font-semibold tracking-wide text-pink-600">
+                  Log out
+                </Text>
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
       </SafeAreaView>
